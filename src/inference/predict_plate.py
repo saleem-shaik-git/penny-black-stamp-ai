6d3ce65ceefafd15de4f8594e0050ef51f7b0381
saleem-shaik-git <79854677+saleem-shaik-git@users.noreply.github.com>
2026-02-07 02:12:21 +0000

    Add inference entry point

--- src/inference/predict_plate.py
+++ src/inference/predict_plate.py
@@ -1,1 +1,11 @@
 
+"""
+Inference entry point for Penny Black plate identification
+Author: Saleem Shaik
+"""
+
+from model.plate_classifier import PlateClassifier
+
+def predict_plate(features):
+    classifier = PlateClassifier()
+    return classifier.predict(features)
